946e5e229ea853a5c7fa5aa88f6f712e2dee336f
Dexx8844 <chideraaslem@gmail.com>
2025-02-24 11:26:14 +0100

    update

--- controller/adminController.js
+++ controller/adminController.js
@@ -207,3 +207,29 @@ exports.updateTeacher = async (req, res) =>{
     }
 }
 
+exports.deleteStudent = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        // Check if the student exists
+        const findStudent = await studentModel.findById(id);
+        if (!findStudent) {
+            return res.status(404).json({
+                message: "Student Not Found"
+            });
+        }
+
+        // Delete the student
+        await studentModel.findByIdAndDelete(id);
+
+        return res.status(200).json({
+            message: "Student Successfully Deleted"
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Internal Server Error: " + err.message,
+        });
+    }
+};
+ 
