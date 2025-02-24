c445847d5f9c664cebcaa3a54d3b00405a10c552
kenneth2222 <obusco4lyfe@gmail.com>
2025-02-24 12:15:00 +0100

    New Update

--- route/adminRouter.js
+++ route/adminRouter.js
@@ -9,12 +9,12 @@ adminRouter.post('/student', adminRole, createStudent);
 adminRouter.post('/teacher', adminRole, createTeacher);
 adminRouter.post('/admin', adminRole, createAdmin);
 adminRouter.post('/login', userLogin)
-// adminRouter.get('/students', checkRole, getAllStudents)
-// adminRouter.get('/teachers', adminRole, getAllTeachers)
+adminRouter.get('/students', checkRole, getAllStudents)
+adminRouter.get('/teachers', adminRole, getAllTeachers)
 adminRouter.put('/student/:id', checkRole, updateStudent)
 adminRouter.put('/teacher/:id', adminRole, updateTeacher)
-// adminRouter.delete('/teacher/:id', adminRole, deleteTeacher)
-// adminRouter.delete('/student/:id', adminRole, deleteStudent)
+adminRouter.delete('/teacher/:id', adminRole, deleteTeacher)
+adminRouter.delete('/student/:id', adminRole, deleteStudent)
 adminRouter.get('/mail/:id/:token', verifyMail)
 
 module.exports = adminRouter;
